3294023087ec6dcd62a07df443d65b3728cc7039
xize <glucassen@live.nl>
2014-04-27 16:34:41 +0200

    removed final again its not like java:(, added whitelist ;-)

--- index.php
+++ index.php
@@ -1,29 +1,43 @@
 <?php
 
 /*
+ * SpamCrawlerPlus © 27-04-2014 all rights reserved to Guido Lucassen this file is lisenced under the GPL, MIT licenses,
+ * however you are not allowed to use this in commercial use.
  * 
- * SpamCrawlerPlus © 27-04-2014 all rights reserved to Guido Lucassen
- * this file is lisenced under the GPL, MIT licenses, however you are not allowed to use this in commercial use.
+ * this is a easy way to block spambots for 1. visiting your site,
+ * 2. prevent spam by faking the website is dead for all spambots who are listed on stopforumspam!
  * 
- * this is a easy way to block spambots for 1. visiting your site, 2. prevent spam by faking the website is dead for all spambots who are listed on stopforumspam!
  * we designed this especially for xenforo not as a addon but more likely to add in the library/config.php file.
  * however this may also work on other cms's if they extend or include their mysql class almost everywhere.
  * this will support cloudflare aswell but also the normal ip's.
- * 
+ *  
  * since ive did for a small time research how those spambots are working.
- * its better to fake a dead with 404 errors rather than seeing them monitoring the site and when nobody is on trying to perform a capatcha bruteforce with the autolearn feature.
- * this also will lowering content scraping as it is probably also a part of the auto process learning. 
- *
+ * its better to fake a dead with 404 errors rather than seeing them monitoring the site
+ * and when nobody is on and trying to perform a capatcha bruteforce with the autolearn feature.
+ * this also will lowering content scraping as it is probably also a part of the auto process learning.
  */
 
 class SpamCrawlerPlus {
-	private final $ip;
+	private $ip;
 	
 	/**
 	 *
 	 * @author xize
-	 * @param $ip -
-	 *        	the ip adress
+	 * @param add here your hardcoded ip adresses!, this allow them to visit your site.
+	 * @return Array
+	 */
+	private function whitelist() {
+		$array = array (
+				"192.168.0.1",
+				"192.168.0.2" 
+		);
+		return $array;
+	}
+	
+	/**
+	 *
+	 * @author xize
+	 * @param $ip - the ip adress
 	 */
 	public function __construct($ip) {
 		$this->ip = $ip;
@@ -32,8 +46,7 @@ class SpamCrawlerPlus {
 	/**
 	 *
 	 * @author xize
-	 * @param
-	 *        	returns the ip adress
+	 * @param returns the ip adress
 	 * @return String
 	 */
 	public function getIpadress() {
@@ -43,11 +56,13 @@ class SpamCrawlerPlus {
 	/**
 	 *
 	 * @author xize
-	 * @param
-	 *        	returns true if the ip is a spamsource ip
+	 * @param returns true if the ip is a spamsource ip
 	 * @return boolean
 	 */
 	public function isSpamSource() {
+		if (in_array ( $this->ip, $this->whitelist () )) {
+			return false;
+		}
 		$options = array (
 				'http' => array (
 						'user_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3' 
@@ -63,6 +78,12 @@ class SpamCrawlerPlus {
 		$args = $json ["response success=\"true\""] ["appears"];
 		if ($args [1] == "yes") {
 			return true;
+		} else {
+			//search if they are possible a dedicated server, even though this could be spoofed easily.
+			$hostname = gethostbyaddr ( $this->ip );
+			if (strpos ( $hostname, "ovh.com" ) || strpos ( $hostname, "ovh.net" ) || strpos ( $hostname, "vps" ) || strpos ( $hostname, "mail.ru" ) || strpos ( $hostname, "ovh.com" ) || strpos ( $hostname, "softlayer" ) || strpos ( $hostname, "vpn" ) || strpos ( $hostname, "dedicated" )) {
+				return true;
+			}
 		}
 		return false;
 	}
@@ -70,8 +91,7 @@ class SpamCrawlerPlus {
 	/**
 	 *
 	 * @author xize
-	 * @param
-	 *        	sents a 404 header, so the bot thinks the page does not exist.
+	 * @param sents a 404 header, so the bot thinks the page does not exist.
 	 */
 	public function sent404Header() {
 		header ( "HTTP/1.0 404 Not Found" );
